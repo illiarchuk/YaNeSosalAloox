87c74deaf8cdaccd3d00144f6283ca13f527b8d5
illiarchuk <illiarchuk@gmail.com>
2025-02-14 05:00:36 +0200

    Первая загрузка проекта

---
+++ YaNeSosalAloo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace YaNeSosalAloo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ YaNeSosalAloo/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using YaNeSosalAloo;
+
+namespace YaNeSosalAloo;
+
+public partial class MainPage : ContentPage
+{
+    private int _clickCount = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();  // Этот вызов связывает код с XAML
+    }
+
+    // Метод для обработки нажатия на кнопку
+    private void OnCounterClicked(object sender, EventArgs e)
+    {
+        _clickCount++;
+        // Убедись, что CounterLabel доступен в контексте
+        CounterLabel.Text = $"Отсосано {_clickCount} раз(а)";
+    }
+}
